c9c1131dfd7c41084fd5bf6955852e558fa96396
mj <54505914+whalsrb100@users.noreply.github.com>
2024-03-13 09:10:21 +0900

    Update pxe_all-in-one.sh

--- pxe_all-in-one.sh
+++ pxe_all-in-one.sh
@@ -13,6 +13,10 @@ NEXT_SERVER=1.2.3.2       ### TFTP-SERVER IP
 MyImageName='oh_my_pxe_image'     ### 이미지이름
 POD_NAME='oh_my_pxe'          ### 파드이름(컨테이너이름)
 MyTag='v0.1'             ### 태그명
+
+SET_TFTP_SERVER="true"
+SET_DHCP_SERVER="true"
+SET_HTTP_SERVER="true"
 ################################################################
 HTTPD_PORT=80
 DHCPD_PORT1=67
@@ -31,14 +35,14 @@ DHCPD_PKG=
 HTTPD_PKG=
 [ "${SET_TFTP_SERVER}" == "true" ] && TFTPD_PKG="tftp-hpa"
 [ "${SET_DHCP_SERVER}" == "true" ] && DHCPD_PKG="dhcp"
-[ "${SET_HTTPD_SERVER}" == "true" ] && HTTPD_PKG="apache2"
+[ "${SET_HTTP_SERVER}" == "true" ] && HTTPD_PKG="apache2"
 INSTALL_PKG="${TFTP_PKG} ${HTTPD_PKG} ${DHCPD_PKG}"
 
 HTTPD_PRE=
 DHCPD_PRE=
-[ "${SET_DHCP_SERVER}" == "true" ] &&  DHCPD_PRE='touch /var/lib/dhcp/dhcpd.leases; chown dhcp:dhcp /var/lib/dhcp/dhcpd.leases'
-[ "${SET_HTTPD_SERVER}" == "true" ] && HTTPD_PRE='sed -i "s/^#ServerName .*/ServerName localhost/g" /etc/apache2/httpd.conf'
-PRE_CMD="${HTTPD_PRE};${DHCPD_PRE}"
+[ "${SET_DHCP_SERVER}" == "true" ] &&  DHCPD_PRE='touch /var/lib/dhcp/dhcpd.leases; chown dhcp:dhcp /var/lib/dhcp/dhcpd.leases;'
+[ "${SET_HTTP_SERVER}" == "true" ] && HTTPD_PRE='sed -i "s/^#ServerName .*/ServerName localhost/g" /etc/apache2/httpd.conf;'
+PRE_CMD="${HTTPD_PRE}${DHCPD_PRE}"
 
 [ "${SET_DHCP_SERVER}" == "true" ] &&  DHCPD_CONF="allow booting;\nallow bootp;\n\nddns-update-style none;\ndefault-lease-time ${DEFAULT_LEASES_TIME};\nmax-lease-time ${MAX_LEASES_TIME};\n\noption subnet-mask ${SUBNET};\noption routers ${ROUTERS};\n\n#option magic code 208 = string;\n#option configfile code 209 = text;\n#option pathprefix code 210 = text;\n#option reboottime code 211 = unsigned integer 32;\noption arch code 93 = unsigned integer 16; #RFC4578\n\nsubnet ${NETWORK} netmask ${SUBNET}\n{\n        range ${RANGE_START} ${RANGE_END};\n        next-server ${NEXT_SERVER};\n\n        if option arch = 00:07 {\n            filename \\\"/pxelinux/grubx64.efi\\\";\n        }else{\n            filename \\\"pxelinux.0\\\";\n        }\n}"
 
@@ -48,7 +52,7 @@ DHCPD_EXEC=
 HTTPD_EXEC=
 [ "${SET_TFTP_SERVER}" == "true" ] && TFTPD_EXEC='in.tftpd -4 -v -L -s /var/tftpboot &'
 [ "${SET_DHCP_SERVER}" == "true" ] &&  DHCPD_EXEC='/usr/sbin/dhcpd -f -cf /etc/dhcp/dhcpd.conf -user dhcp -group dhcp --no-pid &'
-[ "${SET_HTTPD_SERVER}" == "true" ] && HTTPD_EXEC='httpd'
+[ "${SET_HTTP_SERVER}" == "true" ] && HTTPD_EXEC='httpd'
 ENTRYPOINT_EXEC='sleep infinity'
 
 ENTRYPOINT_CMD="${SHABANG}\n${TFTPD_EXEC}\n${DHCPD_EXEC}\n${HTTPD_EXEC}\n${ENTRYPOINT_EXEC}"
@@ -57,7 +61,7 @@ ENTRYPOINT_PERMISSION_SET="chmod 755 ${ENTRYPOINT_NAME}"
 
 echo "FROM ${IMAGE_origin}" > Dockerfile
 echo -n "RUN apk add ${INSTALL_PKG} && " >> Dockerfile
-echo -n "${PRE_CMD}; " >> Dockerfile
+echo -n "${PRE_CMD} " >> Dockerfile
 [ "${SET_DHCP_SERVER}" == "true" ] && echo -n "echo -e \"$(echo ${DHCPD_CONF})\" && " >> Dockerfile
 echo -n "echo ${ENTRYPOINT_CMD} > ${ENTRYPOINT_NAME} && " >> Dockerfile
 echo "${ENTRYPOINT_PERMISSION_SET}" >> Dockerfile
