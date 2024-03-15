8684f543e9d862dd8196519b1f801f051c5b6dcb
mj <54505914+whalsrb100@users.noreply.github.com>
2024-03-15 17:42:34 +0900

    Update create_pxe_image.sh

--- create_pxe_image.sh
+++ create_pxe_image.sh
@@ -62,7 +62,7 @@ podman rmi -f ${IMAGE_custom}:${MyTag} > /dev/null 2>&1
 ################################################################
 # Create Settings
 ################################################################
-[ "${USE_TFTP_SERVER}" == "true" ] && TFTPD_PKG="tftp-hpa"
+[ "${USE_TFTP_SERVER}" == "true" ] && TFTPD_PKG="tftp-hpa syslinux"
 [ "${USE_DHCP_SERVER}" == "true" ] && DHCPD_PKG="dhcp"
 [ "${USE_HTTP_SERVER}" == "true" ] && HTTPD_PKG="apache2"
 INSTALL_PKG="${TFTPD_PKG} ${HTTPD_PKG} ${DHCPD_PKG}"
@@ -71,15 +71,27 @@ INSTALL_PKG="${TFTPD_PKG} ${HTTPD_PKG} ${DHCPD_PKG}"
 [ "${USE_HTTP_SERVER}" == "true" ] && HTTPD_PRE='sed -i "s/^#ServerName .*/ServerName localhost/g" /etc/apache2/httpd.conf;'
 PRE_CMD="${HTTPD_PRE}${DHCPD_PRE}"
 
-[ "${USE_DHCP_SERVER}" == "true" ] && DHCPD_CONF="allow booting;\nallow bootp;\n\nddns-update-style none;\ndefault-lease-time ${DEFAULT_LEASES_TIME};\nmax-lease-time ${MAX_LEASES_TIME};\n\noption subnet-mask ${SUBNET};\noption routers ${ROUTERS};\n\n#option magic code 208 = string;\n#option configfile code 209 = text;\n#option pathprefix code 210 = text;\n#option reboottime code 211 = unsigned integer 32;\noption arch code 93 = unsigned integer 16; #RFC4578\n\nsubnet ${NETWORK} netmask ${SUBNET}\n{\n        range ${RANGE_START} ${RANGE_END};\n        next-server ${NEXT_SERVER};\n\n        if option arch = 00:07 {\n            filename \\\"/pxelinux/grubx64.efi\\\";\n        }else{\n            filename \\\"pxelinux.0\\\";\n        }\n}"
+[ "${USE_DHCP_SERVER}" == "true" ] && GRUB_EFI_FILE='/grubx64.efi'
+[ "${USE_DHCP_SERVER}" == "true" ] && GRUB_LEGACY_FILE='pxelinux.0'
+[ "${USE_DHCP_SERVER}" == "true" ] && DHCPD_CONF="allow booting;\nallow bootp;\n\nddns-update-style none;\ndefault-lease-time ${DEFAULT_LEASES_TIME};\nmax-lease-time ${MAX_LEASES_TIME};\n\noption subnet-mask ${SUBNET};\noption routers ${ROUTERS};\n\n#option magic code 208 = string;\n#option configfile code 209 = text;\n#option pathprefix code 210 = text;\n#option reboottime code 211 = unsigned integer 32;\noption arch code 93 = unsigned integer 16; #RFC4578\n\nsubnet ${NETWORK} netmask ${SUBNET}\n{\n        range ${RANGE_START} ${RANGE_END};\n        next-server ${NEXT_SERVER};\n\n        if option arch = 00:07 {\n            filename \\\"${GRUB_EFI_FILE}\\\";\n        }else{\n            filename \\\"${GRUB_LEGACY_FILE}\\\";\n        }\n}"
 
-SHABANG='#!/bin/sh'
-[ "${USE_TFTP_SERVER}" == "true" ] && TFTPD_EXEC='in.tftpd -4 -v -L -s /var/tftpboot &'
-[ "${USE_DHCP_SERVER}" == "true" ] && DHCPD_EXEC='/usr/sbin/dhcpd -f -cf /etc/dhcp/dhcpd.conf -user dhcp -group dhcp --no-pid &'
-[ "${USE_HTTP_SERVER}" == "true" ] && HTTPD_EXEC='httpd'
-ENTRYPOINT_EXEC='sleep infinity'
+ENTRYPOINT_CMD='#!/bin/sh'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -d "/var/tftpboot/pxelinux.cfg"  ] && mkdir /var/tftpboot/pxelinux.cfg/'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -f "/var/tftpboot/ldlinux.c32"  ] && cp /usr/share/syslinux/ldlinux.c32  /var/tftpboot/'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -f "/var/tftpboot/libcom32.c32" ] && cp /usr/share/syslinux/libcom32.c32 /var/tftpboot/'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -f "/var/tftpboot/libutil.c32"  ] && cp /usr/share/syslinux/libutil.c32  /var/tftpboot/'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -f "/var/tftpboot/memdisk"      ] && cp /usr/share/syslinux/memdisk      /var/tftpboot/'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -f "/var/tftpboot/menu.c32"     ] && cp /usr/share/syslinux/menu.c32     /var/tftpboot/'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -f "/var/tftpboot/pxelinux.0"   ] && cp /usr/share/syslinux/pxelinux.0   /var/tftpboot/'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -f "/var/tftpboot/vesamenu.c32" ] && cp /usr/share/syslinux/vesamenu.c32 /var/tftpboot/'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -f "/var/tftpboot/cmenu.c32"    ] && cp /usr/share/syslinux/cmenu.c32    /var/tftpboot/'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='[ ! -f "/var/tftpboot/pxelinux.cfg/default"  ] && echo -e \"default menu.c32\nprompt 0\ntimeout 0\n\nMENU TITLE titleName\nLABEL selectMenu1KERNEL \\\"VMLINUZ FilePath\\\"\nAPPEND ksdevice=bootif initrd=\\\"INITRD IMG Path\\\" network vnc vncconnect=\\\"VNCConnectIP\\\":5500 method/ks=\\\"protocol://Address/URI/kickStartFilePath\\\"\nIPAPPEND 2\" > /var/tftpboot/pxelinux.cfg/default'
+[ "${USE_TFTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='in.tftpd -4 -v -L -s /var/tftpboot &'
+[ "${USE_DHCP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='/usr/sbin/dhcpd -f -cf /etc/dhcp/dhcpd.conf -user dhcp -group dhcp --no-pid &'
+[ "${USE_HTTP_SERVER}" == "true" ] && ENTRYPOINT_CMD+='httpd'
+ENTRYPOINT_CMD+='sleep infinity'
 
-ENTRYPOINT_CMD="${SHABANG}\n${TFTPD_EXEC}\n${DHCPD_EXEC}\n${HTTPD_EXEC}\n${ENTRYPOINT_EXEC}"
+#ENTRYPOINT_CMD="${SHABANG}\n${TFTPD_EXEC_01}\n${TFTPD_EXEC_02}\n${TFTPD_EXEC_03}\n${TFTPD_EXEC_04}\n${TFTPD_EXEC_05}\n${TFTPD_EXEC_06}\n${TFTPD_EXEC_07}\n${TFTPD_EXEC_08}\n${TFTPD_EXEC_09}\n${TFTPD_EXEC_10}\n${TFTPD_EXEC_11}\n${DHCPD_EXEC}\n${HTTPD_EXEC}\n${ENTRYPOINT_EXEC}"
 ENTRYPOINT_NAME='/entrypoint.sh'
 ENTRYPOINT_PERMISSION_SET="chmod 755 ${ENTRYPOINT_NAME}"
 ################################################################
@@ -100,6 +112,14 @@ podman rmi ${IMAGE_origin}
 
 IMG_NAME="${IMAGE_custom}:${MyTag}"
 echo "#=================================================="
+([ "${USE_DHCP_SERVER}" == "true" ] || [ "${USE_TFTP_SERVER}" == "true" ]) && echo -e "\n\n\n"
+([ "${USE_DHCP_SERVER}" == "true" ] || [ "${USE_TFTP_SERVER}" == "true" ]) && echo '#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+([ "${USE_DHCP_SERVER}" == "true" ] || [ "${USE_TFTP_SERVER}" == "true" ]) && echo "You Need Check Exists files(1/2). - For Legacy: ${TFTPBOOT_HOME_DIR}/${GRUB_EFI_FILE}"
+([ "${USE_DHCP_SERVER}" == "true" ] || [ "${USE_TFTP_SERVER}" == "true" ]) && echo "You Need Check Exists files(2/2). - For UEFI: ${TFTPBOOT_HOME_DIR}${GRUB_LEGACY_FILE}"
+([ "${USE_DHCP_SERVER}" == "true" ] || [ "${USE_TFTP_SERVER}" == "true" ]) && echo '#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+([ "${USE_DHCP_SERVER}" == "true" ] || [ "${USE_TFTP_SERVER}" == "true" ]) && echo -e "[Check Command] # podman exec -it ${POD_NAME} ls -l --color ${TFTPBOOT_HOME_DIR}/"
+([ "${USE_DHCP_SERVER}" == "true" ] || [ "${USE_TFTP_SERVER}" == "true" ]) && echo -e "\n\n\n"
+
 PrintCmds="podman run --privileged -itd --rm"
 [ "${USE_TFTP_SERVER}" == "true" ] && PrintCmds+=" -v ${TFTPBOOT_HOME_DIR}:/var/tftpboot -p ${EXT_TFTP_PORT}:69 "
 [ "${USE_HTTP_SERVER}" == "true" ] && PrintCmds+=" -v ${HTTPD_DOCUMENT_DIR}:/var/www/localhost/htdocs -p ${EXT_HTTPD_PORT}:80 "
